21df946224510d1dd6ca8f17db8e543fcaba2e21
Tejesh Sakhamuri <thanmaisakhamuri999@gmail.com>
2026-04-18 19:07:19 +0530

    Add: longest-common-prefix - submission-2

---
+++ Data Structures & Algorithms/longest-common-prefix/submission-2.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        for(int i=0;i<strs[0].size();i++)
+        {
+            char ref=strs[0][i];
+            bool unmatch=false;
+            for(int j=0;j<strs.size();j++)
+            {
+               if(i >= strs[j].size() || strs[j][i]!=ref) 
+               {
+                 unmatch =true;
+                 break;
+               } 
+            }
+            if(unmatch)
+            {
+                return strs[0].substr(0,i);
+            }
+        }
+        return strs[0];
+    }
+};
