8e801b09891864d207914830a2a00d14bfe64638
Tejesh Sakhamuri <thanmaisakhamuri999@gmail.com>
2026-05-04 23:32:46 +0530

    Add: frequency-of-the-most-frequent-element - submission-2

---
+++ Data Structures & Algorithms/frequency-of-the-most-frequent-element/submission-2.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int maxFrequency(vector<int>& nums, int k) {
+        sort(nums.begin(), nums.end());
+        
+        long long totalSum = 0;
+        int left = 0;
+        int maxFreq = 0;
+        
+        for (int right = 0; right < nums.size(); ++right) {
+            totalSum += nums[right];
+
+            while ((long long)nums[right] * (right - left + 1) > k + totalSum) {
+                totalSum -= nums[left];
+                left++;
+            }
+            
+            maxFreq = max(maxFreq, right - left + 1);
+        }
+        
+        return maxFreq;
+    }
+};
