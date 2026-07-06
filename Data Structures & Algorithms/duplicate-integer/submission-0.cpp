4f239df90f4a1e0bef41530cffe83eb60a9f8c2a
Tejesh Sakhamuri <thanmaisakhamuri999@gmail.com>
2026-07-06 16:43:12 +0530

    Add: duplicate-integer - submission-0

---
+++ Data Structures & Algorithms/duplicate-integer/submission-0.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool hasDuplicate(vector<int>& nums) {
+        unordered_map<int,int> m;
+        for(int i=0;i<nums.size();i++)
+        {
+            if(m[nums[i]]>0) return true;
+            m[nums[i]]++;
+        }
+        return false;
+    }
+};
