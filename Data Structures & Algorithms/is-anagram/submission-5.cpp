de7f298672b4a31c1ac6af8cdd52a142bd857038
Tejesh Sakhamuri <thanmaisakhamuri999@gmail.com>
2026-07-07 13:33:50 +0530

    Add: is-anagram - submission-5

---
+++ Data Structures & Algorithms/is-anagram/submission-5.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.size()!=t.size()) return false;
+        unordered_map<char,int> m;
+        for(int i=0;i<s.size();i++)
+        {
+            m[s[i]]++;
+        }
+        for(int i=0;i<t.size();i++)
+        {
+            m[t[i]]--;
+            if(m[t[i]]<0) return false;
+        }
+        for(auto it=m.begin();it!=m.end();it++)
+        {
+            if(it->second>0) return false;
+        }
+        return true;
+    }
+};
