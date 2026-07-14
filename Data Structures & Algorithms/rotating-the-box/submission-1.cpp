593daa453904a5f804aee5e8d32f5a2c1dac62da
Tejesh Sakhamuri <thanmaisakhamuri999@gmail.com>
2026-07-14 12:43:12 +0530

    Add: rotating-the-box - submission-1

---
+++ Data Structures & Algorithms/rotating-the-box/submission-1.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    vector<vector<char>> rotateTheBox(vector<vector<char>>& boxGrid) {
+        vector<vector<char>> ans(boxGrid[0].size(),vector<char>(boxGrid.size()));
+        for(int i=0;i<boxGrid.size();i++)
+        {
+            int fallable_pos=-1;
+            for(int j=boxGrid[0].size()-1;j>=0;j--)
+            {
+                if(boxGrid[i][j]=='.') fallable_pos=max(j,fallable_pos);
+                else if(boxGrid[i][j]=='*') fallable_pos=-1;
+                else
+                {
+                    if(fallable_pos!=-1) 
+                    {
+                        swap(boxGrid[i][j],boxGrid[i][fallable_pos]);
+                        j=fallable_pos;
+                        fallable_pos=-1;
+                    }
+                }
+                ans[j][boxGrid.size()-i-1]=boxGrid[i][j]; 
+            }
+        }
+        // for(int i=0;i<boxGrid.size();i++)
+        // {
+        //     for(int j=0;j<boxGrid[i].size();j++)
+        //     {
+        //         ans[j][boxGrid.size()-i-1]=boxGrid[i][j];
+        //     }
+        // }
+        return ans;
+    }
+};
